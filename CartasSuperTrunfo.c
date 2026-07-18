26a6717b1f9d8e50f8a791b59fe5f0d9a7e9fb82
higosantana <81244189+higosantana@users.noreply.github.com>
2026-07-18 13:16:25 +0000

    Adicionando Codigo inicial, em andamento!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,53 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+    char estado[1]; // Declara variavel estado
+    char codigo[3]; //Declara variavel codigo da carta
+    char cidade[50]; //Declara Nome da Cidade
+    int populacao; //Declara tamanho População
+    float area; //Declara Área territorial em Km/2
+    float pib; //Declara PIB
+    int pontos; //Declara pontos turísticos
+
   // Área para entrada de dados
+
+    printf("Carta 1\n");
+    
+    printf("Digite a letra do Estado: \n");
+    scanf("%s", &estado);
+    
+    printf("Digite o Código do Estado: \n");
+    scanf("%s", &codigo);
+    
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite tamanho da população: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a Área territorial, \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib);
+
+    printf("Digite a quantidade de pontos turístico: \n");
+    scanf("%d", &pontos);
+
+
 
   // Área para exibição dos dados da cidade
 
-return 0;
+    printf("Carta 1\n");
+    printf("Estado: %s\n", estado);
+    printf("Código: %s\n", codigo);
+    printf("Nome da Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área:%f\n", area);
+    printf("PIB: %f\n", pib);
+    printf("Número de pontos turísticos: %d\n", pontos);
+
+
+
+    return 0;
 } 
